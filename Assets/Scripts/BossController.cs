422960935f1b01a1a2836b4584210bd03884e85e
Cameron <cameron.meyer02@gmail.com>
2021-09-29 00:57:58 -0500

    Smooth Boss Movement

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -26,12 +26,14 @@ public class BossController : MonoBehaviour
 
     [SerializeField] GameObject _ground;
 
-    [SerializeField] GameObject _patrolPositionsObj;
-    private List<Vector3> _patrolPositions;
+    [SerializeField] Transform LowerLeftBound;
+    [SerializeField] Transform UpperRightBound;
+    [SerializeField] float _telegraphDuration = 1f;
     private float _timeToNextMovement;
     private Vector3 _nextLocation;
     private bool _isPatroling = false;
     private bool _isRushing = false;
+    private bool _isTelegraphing = false;
 
     public float MoveSpeed
     {
@@ -44,16 +46,6 @@ public class BossController : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
-    }
-
-    private void Start()
-    {
-        _patrolPositions = new List<Vector3>();
-        foreach(Transform child in _patrolPositionsObj.transform)
-        {
-            _patrolPositions.Add(child.position);
-        }
-        
     }
 
     private void Update()
@@ -82,22 +74,28 @@ public class BossController : MonoBehaviour
             {
                 case 0:
                     _nextLocation = transform.position;
+                    _timeToNextMovement = Time.time + Random.Range(1.2f, 3f);
                     Debug.Log("Idle Now");
                     break;
                 case 1:
-                    _nextLocation = _patrolPositions[Random.Range(0, _patrolPositions.Count)];
+                    _nextLocation = new Vector3(UnityEngine.Random.Range(LowerLeftBound.position.x, UpperRightBound.position.x),
+                                                UnityEngine.Random.Range(LowerLeftBound.position.y, UpperRightBound.position.y),
+                                                UnityEngine.Random.Range(LowerLeftBound.position.z, UpperRightBound.position.z));
+                    _timeToNextMovement = Time.time + Random.Range(2.2f, 4f);
                     _isPatroling = true;
+                    StartCoroutine(rotateBase((_nextLocation - transform.position), _telegraphDuration));
                     Debug.Log("Patroling Now");
                     break;
                 case 2:
                     _nextLocation = _player.transform.position;
+                    _timeToNextMovement = Time.time + 3.2f;
                     _isRushing = true;
                     Debug.Log("Rushing Now");
                     break;
             }
 
             // randomly pick how long we will perform the movement action for
-            _timeToNextMovement = Time.time + Random.Range(1.2f, 3f);
+            //_timeToNextMovement = Time.time + Random.Range(1.2f, 3f);
         }
 
         MoveTank();
@@ -106,16 +104,62 @@ public class BossController : MonoBehaviour
     private void MoveTank()
     {
         Vector3 move = _nextLocation - transform.position;
+        Vector3 normMove = move;
+        normMove.Normalize();
 
-        //Debug.Log(move);
+        // don't move the object if it's already within the position threshold
+        if (move.magnitude <= (normMove * _moveSpeed).magnitude) // if the distance vector is less than the amount we can move in 1 frame
+        {
+            if (_isPatroling && !_isTelegraphing) // calculate a new point to patrol to
+            {
+                _nextLocation = new Vector3(UnityEngine.Random.Range(LowerLeftBound.position.x, UpperRightBound.position.x),
+                                            UnityEngine.Random.Range(LowerLeftBound.position.y, UpperRightBound.position.y),
+                                            UnityEngine.Random.Range(LowerLeftBound.position.z, UpperRightBound.position.z));
+                StartCoroutine(rotateBase((_nextLocation - transform.position), _telegraphDuration));
+            }
+            else
+            {
+                return; // don't move if we're already basically in position
+            }
+        }
+        else if (!_isTelegraphing && (_isPatroling || _isRushing)) // actually translate and rotate the object if it's supposed to move
+        {
+            transform.position += normMove * _moveSpeed;
 
-        if(move.magnitude <= 0.1f) { return; } // don't move if we're already basically in position
+            StartCoroutine(rotateBase(move, 0));
+        }
+    }
 
-        move.Normalize();
+    private IEnumerator rotateBase(Vector3 direction, float duration)
+    {
+        if (duration == 0f)
+        {
+            _base.transform.rotation = Quaternion.LookRotation(direction);
+            yield return null;
+        }
 
-        transform.position += move * _moveSpeed;
+        _timeToNextMovement += duration;
 
-        _base.transform.rotation = Quaternion.LookRotation(move);
+        float elapsedTime = 0;
+        Quaternion initValue = _base.transform.rotation;
+        Quaternion target = Quaternion.LookRotation(direction);
+
+        Vector3 tempNextLocation = _nextLocation;
+        _nextLocation = transform.position;
+
+        while (elapsedTime < duration)
+        {
+            _isTelegraphing = true;
+            _base.transform.rotation = Quaternion.Slerp(
+                                            initValue,
+                                            target,
+                                            (elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        _isTelegraphing = false;
+        _nextLocation = tempNextLocation;
     }
 
     private void TurnTurret()
